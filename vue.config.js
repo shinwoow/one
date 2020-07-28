a548f9c300015d70675bda294df4a7e95eceae21
shinwoow <1109323254@qq.com>
2020-07-28 10:13:23 +0800

    配置修改
    配置修改

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 // vue.config.js
+const path = require("path");
+
+const resolve = dir => {
+  return path.join(__dirname, dir);
+};
+
 module.exports = {
   publicPath: "/",
   outputDir: "dist",
@@ -7,16 +13,37 @@ module.exports = {
   filenameHashing: true,
   runtimeCompiler: false,
   transpileDependencies: [],
-  configureWebpack: {},
-  chainWebpack: {},
-  css: {
-    loaderOptions: {
-      css: {
-        modules: {
-          localIdentName: "[name]-[hash]"
-        },
-        localscONVENTION: "camelCaseOnly"
-      }
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("_c", resolve("src/components"))
+      .set("_css", resolve("src/assets/css"));
+  },
+
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [
+        path.resolve(__dirname, "./src/assets/css/mixins.scss"),
+        path.resolve(__dirname, "./src/assets/css/variable.scss")
+      ]
     }
-  }
-}
+  },
+  devServer: {
+    open: true,
+    host: "127.0.0.1",
+    port: 7777,
+    https: false,
+    // proxy: {
+    //   "/api": {
+    //     target: "http://",
+    //     changeOrigin: true
+    //   }
+		// }
+		overlay: {
+			warnings: false,
+			errors: false 
+		}
+	},
+	lintOnSave: false
+};
